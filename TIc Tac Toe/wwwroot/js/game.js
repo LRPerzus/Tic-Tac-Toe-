20f5b38ebed142abc1c6a2cc40bc565a713e5fd3
LRPerzus <s10242179@connect.np.edu.sg>
2023-09-28 15:33:46 +0800

    Full Tic Tac Toe Complete for local version

--- TIc Tac Toe/wwwroot/js/game.js
+++ TIc Tac Toe/wwwroot/js/game.js
@@ -1,24 +1,112 @@
-﻿let turn = 1;
+﻿function check(symbol) {
+    if ($(".square#1").hasClass(symbol) &&
+        $(".square#2").hasClass(symbol) &&
+        $(".square#3").hasClass(symbol)) {
+        $(".square#1").css("color", "green");
+        $(".square#2").css("color", "green");
+        $(".square#3").css("color", "green");
+        return true;
+    } else if ($(".square#4").hasClass(symbol)
+        && $(".square#5").hasClass(symbol)
+        && $(".square#6").hasClass(symbol)) {
+        $(".square#4").css("color", "green");
+        $(".square#5").css("color", "green");
+        $(".square#6").css("color", "green");
+        return true;
+    } else if ($(".square#7").hasClass(symbol)
+        && $(".square#8").hasClass(symbol)
+        && $(".square#9").hasClass(symbol)) {
+        $(".square#7").css("color", "green");
+        $(".square#8").css("color", "green");
+        $(".square#9").css("color", "green");
+        return true;
+    } else if ($(".square#1").hasClass(symbol)
+        && $(".square#4").hasClass(symbol)
+        && $(".square#7").hasClass(symbol)) {
+        $(".square#1").css("color", "green");
+        $(".square#4").css("color", "green");
+        $(".square#7").css("color", "green");
+        return true;
+    } else if ($(".square#2").hasClass(symbol)
+        && $(".square#5").hasClass(symbol)
+        && $(".square#8").hasClass(symbol)) {
+        $(".square#2").css("color", "green");
+        $(".square#5").css("color", "green");
+        $(".square#8").css("color", "green");
+        return true;
+    } else if ($(".square#3").hasClass(symbol)
+        && $(".square#6").hasClass(symbol)
+        && $(".square#9").hasClass(symbol)) {
+        $(".square#3").css("color", "green");
+        $(".square#6").css("color", "green");
+        $(".square#9").css("color", "green");
+        return true;
+    } else if ($(".square#1").hasClass(symbol)
+        && $(".square#5").hasClass(symbol)
+        && $(".square#9").hasClass(symbol)) {
+        $(".square#1").css("color", "green");
+        $(".square#5").css("color", "green");
+        $(".square#9").css("color", "green");
+        return true;
+    } else if ($(".square#3").hasClass(symbol)
+        && $(".square#5").hasClass(symbol)
+        && $(".square#7").hasClass(symbol)) {
+        $(".square#3").css("color", "green");
+        $(".square#5").css("color", "green");
+        $(".square#7").css("color", "green");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+let turn = 1;
 let moveNotation = "";
 $("button").click(function () {
 
     //Getting the class name front
-    var className = $(this).attr("class");
+    var className = $(this).attr("id");
 
     if (turn == 1) {
         $("#screen").text("PLAYER 1 TURN");
 
         // Check sign font from font-awesome
         $(this).addClass("circle");
-        moveNotation += "O"+className
-        turn = 2;
+        if (check("circle"))
+        {
+            moveNotation += "O" + className;
+            $(".square").prop("disabled", true);
+            $("#screen").text("PLAYER 1 Won");
+
+
+        }
+        else
+        {
+            moveNotation += "O" + className + ",";
+            turn = 2;
+        }
+        
     }
     else {
         $("#screen").text("PLAYER 2 TURN"); // Corrected the string
         // Cross sign font from font-awesome
         $(this).addClass("cross");
-        moveNotation += "X" + className
-        turn = 1;
+        if (check("cross"))
+        {
+            moveNotation += "X" + className;
+            $(".square").prop("disabled", true);
+            $("#screen").text("PLAYER 2 Won");
+
+
+        }
+        else {
+            moveNotation += "X" + className + ",";
+            turn = 1;
+        }
+        
+
     }
 
     // Disable the button after a symbol is placed
